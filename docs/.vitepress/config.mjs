71bc4b3ad167cf0009eb2135b93903b2078b1bc9
Arid <118916772+AridDev@users.noreply.github.com>
2024-01-29 22:27:39 +0600

    Using default docs theme

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,26 +3,17 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
 	title: "Arid's portfolio",
+	description: "A VitePress Site",
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [
-			{ text: 'Home', link: '/' },
-			{ text: 'Examples', link: '/markdown-examples' },
+			{ text: "Home", link: "/" },
 			{ text: "Parallelity", link: "/parallelity" }
 		],
 
-		sidebar: [
-			{
-				text: 'Examples',
-				items: [
-					{ text: 'Markdown Examples', link: '/markdown-examples' },
-					{ text: 'Runtime API Examples', link: '/api-examples' }
-				]
-			}
-		],
 
 		socialLinks: [
-			{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+			{ icon: "github", link: "https://github.com/AridDev/AridDev.github.io" }
 		]
 	}
 })
